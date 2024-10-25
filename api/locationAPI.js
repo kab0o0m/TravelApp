3b3928f02734895549574e1976b94340043cdb3b
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2024-10-25 18:23:49 +0800

    integrate remove-location API

--- api/locationAPI.js
+++ api/locationAPI.js
@@ -47,11 +47,30 @@ export const fetchSavedLocations = async (user_id) => {
     
 
     const result = await response.json();
-
-    return result.savedLocations; // Assuming saved locations are under "saved-locations"
     
+    return result.savedLocations; // Assuming saved locations are under "saved-locations"
+
   } catch (error) {
     console.error("[API] Error fetching saved locations:", error);
+    throw error;
+  }
+};
+
+export const removeSavedLocation = async (savedLocationId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/api/remove-saved-location`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: savedLocationId }), // Send the saved_location ID
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to remove saved location: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error removing saved location:", error);
     throw error;
   }
 };
