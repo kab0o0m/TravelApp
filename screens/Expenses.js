c7f5107b139e80aff7fb79166504b5940c0ef0cb
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2024-11-02 19:00:37 +0800

    fix UI

--- screens/Expenses.js
+++ screens/Expenses.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Dimensions, ScrollView, Alert, Animated, Easing, TouchableOpacity } from 'react-native';
-import { useFonts, Nunito_400Regular, Nunito_700Bold, Nunito_900Black, } from '@expo-google-fonts/nunito';
-import { Picker } from '@react-native-picker/picker'; 
-import Button from "../components/Button"; 
-import AddExpenseModal from './AddExpenseModal';
-import * as Progress from 'react-native-progress';
-import RoundedSquareIcon from '../components/RoundedSquareIcon';
-import { fetchExpenses } from '../api/expensesAPI';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  Alert,
+  Animated,
+  Easing,
+  TouchableOpacity,
+} from "react-native";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+  Nunito_900Black,
+} from "@expo-google-fonts/nunito";
+import { Picker } from "@react-native-picker/picker";
+import Button from "../components/Button";
+import AddExpenseModal from "./AddExpenseModal";
+import * as Progress from "react-native-progress";
+import RoundedSquareIcon from "../components/RoundedSquareIcon";
+import { fetchExpenses } from "../api/expensesAPI";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { fetchUserData } from "../api/authAPI";
-import { PieChart } from 'react-native-chart-kit'; // Import the PieChart
-import SetBudgetModal from './SetBudgetModal'; // Import the SetBudgetModal
-import { Swipeable } from 'react-native-gesture-handler';
+import { PieChart } from "react-native-chart-kit"; // Import the PieChart
+import SetBudgetModal from "./SetBudgetModal"; // Import the SetBudgetModal
+import { Swipeable } from "react-native-gesture-handler";
 import NavBar from "../components/NavBar";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
@@ -31,7 +46,7 @@ const Expenses = () => {
   const [userId, setUserId] = useState(null);
   const [summaryVisible, setSummaryVisible] = useState(false);
   const slideAnim = useState(new Animated.Value(0))[0];
-  const [budgetModalVisible, setBudgetModalVisible] = useState(false); 
+  const [budgetModalVisible, setBudgetModalVisible] = useState(false);
 
   useEffect(() => {
     // // Dummy data for testing
@@ -46,7 +61,7 @@ const Expenses = () => {
     // setExpenses(dummyExpenses);
     // const total = dummyExpenses.reduce((sum, expense) => sum + expense.price, 0);
     // setTotalSpent(total);
-    
+
     const loadExpenses = async () => {
       try {
         let storedUserData = await AsyncStorage.getItem("userData");
@@ -62,8 +77,11 @@ const Expenses = () => {
         setUserId(userData.id);
         const fetchedExpenses = await fetchExpenses(userData.id);
         setExpenses(fetchedExpenses);
-  
-        const total = fetchedExpenses.reduce((sum, expense) => sum + expense.amount, 0);
+
+        const total = fetchedExpenses.reduce(
+          (sum, expense) => sum + expense.amount,
+          0
+        );
         setTotalSpent(total);
       } catch (error) {
         console.error("Error loading expenses:", error);
@@ -92,29 +110,29 @@ const Expenses = () => {
       population: 150, // Total amount spent on Food
       color: "#FF6347", // Color for the Food slice
       legendFontColor: "#333", // Color for the legend text
-      legendFontSize: 15 // Font size for the legend text
+      legendFontSize: 15, // Font size for the legend text
     },
     {
       name: "Transport",
       population: 80, // Total amount spent on Transport
       color: "#FFD700", // Color for the Transport slice
       legendFontColor: "#333",
-      legendFontSize: 15
+      legendFontSize: 15,
     },
     {
       name: "Entertainment",
       population: 120, // Total amount spent on Entertainment
       color: "#1E90FF", // Color for the Entertainment slice
       legendFontColor: "#333",
-      legendFontSize: 15
+      legendFontSize: 15,
     },
     {
       name: "Utilities",
       population: 60, // Total amount spent on Utilities
       color: "#32CD32", // Color for the Utilities slice
       legendFontColor: "#333",
-      legendFontSize: 15
-    }
+      legendFontSize: 15,
+    },
   ];
 
   const formatDate = (isoDate) => {
@@ -143,37 +161,37 @@ const Expenses = () => {
   // const handleDeleteExpense = (id) => {
   //   const updatedExpenses = expenses.filter(expense => expense.id !== id);
   //   setExpenses(updatedExpenses);
-    
+
   //   // Update totalSpent
   //   const deletedExpense = expenses.find(expense => expense.id === id);
   //   if (deletedExpense) {
   //     setTotalSpent((prevTotal) => prevTotal + expense.amount);
   //   };
-  
+
   //   const formatDate = (isoDate) => {
   //     const date = new Date(isoDate);
-  
+
   //     if (isNaN(date.getTime())) {
   //       return "Invalid Date"; // Return placeholder if date is invalid
   //     }
-  
+
   //     return new Intl.DateTimeFormat("en-GB", {
   //       day: "numeric",
   //       month: "short",
   //       year: "numeric",
   //     }).format(date);
-  
+
   //   }
   // };
 
   const handleDeleteExpense = (id) => {
-    const updatedExpenses = expenses.filter(expense => expense.id !== id);
+    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
     setExpenses(updatedExpenses);
-    
+
     // Update totalSpent
-    const deletedExpense = expenses.find(expense => expense.id === id);
+    const deletedExpense = expenses.find((expense) => expense.id === id);
     if (deletedExpense) {
-      setTotalSpent(prevTotal => prevTotal - deletedExpense.amount);
+      setTotalSpent((prevTotal) => prevTotal - deletedExpense.amount);
     }
   };
 
@@ -191,43 +209,40 @@ const Expenses = () => {
 
   return (
     <View style={styles.container}>
-  <View style={styles.headerContainer}>
-    <Text style={styles.header}>EXPENSES</Text>
-    <View style={styles.tripContainer}>
-      <Text style={styles.tripText}>Sentosa</Text>
-      <View style={styles.line} />
-    </View>
-  </View>
-
-  <View style={styles.box}>
-    <View style={styles.innerBox}>
-      <Text style={styles.amount}>SGD {totalSpent}</Text>
-
-      {budget === 0 ? (
-        <TouchableOpacity onPress={() => setBudgetModalVisible(true)}>
-          <Text style={styles.budgetText}>Set a budget</Text>
-        </TouchableOpacity>
-      ) : (
-        <View style={styles.progressContainer}>
-          <Progress.Bar 
-            progress={progress} 
-            width={screenWidth * 0.8}
-            color="#F47966"
-            unfilledColor="#61A4AB"
-            height={screenHeight * 0.01}
-            borderWidth={0}
-          />
-          <Text style={styles.progressText}>BUDGET: SGD {budget}</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>EXPENSES</Text>
+        <View style={styles.tripContainer}>
+          <Text style={styles.tripText}>Sentosa</Text>
+          <View style={styles.line} />
         </View>
-      )}
+      </View>
 
+      <View style={styles.box}>
+        <View style={styles.innerBox}>
+          <Text style={styles.amount}>SGD {totalSpent}</Text>
 
-
+          {budget === 0 ? (
+            <TouchableOpacity onPress={() => setBudgetModalVisible(true)}>
+              <Text style={styles.budgetText}>Set a budget</Text>
+            </TouchableOpacity>
+          ) : (
+            <View style={styles.progressContainer}>
+              <Progress.Bar
+                progress={progress}
+                width={screenWidth * 0.8}
+                color="#F47966"
+                unfilledColor="#61A4AB"
+                height={screenHeight * 0.01}
+                borderWidth={0}
+              />
+              <Text style={styles.progressText}>BUDGET: SGD {budget}</Text>
+            </View>
+          )}
 
           <Button
             title="View Summary"
             onPress={toggleSummary}
-            backgroundColor="#006D77" 
+            backgroundColor="#006D77"
             textColor="#FFFFFF"
             paddingVertical={screenHeight * 0.001}
             borderRadius={25}
@@ -263,7 +278,9 @@ const Expenses = () => {
         </View>
       )}
 
-      <Animated.View style={[styles.whiteBox, { transform: [{ translateY: slideAnim }] }]}>
+      <Animated.View
+        style={[styles.whiteBox, { transform: [{ translateY: slideAnim }] }]}
+      >
         <Text style={styles.topLeftText}>Your Expenses</Text>
 
         <View style={styles.sortContainer}>
@@ -285,24 +302,33 @@ const Expenses = () => {
         ) : (
           <ScrollView>
             {expenses.map((expense) => (
-              <Swipeable key={expense.id} renderRightActions={() => renderRightActions(expense.id)}>
+              <Swipeable
+                key={expense.id}
+                renderRightActions={() => renderRightActions(expense.id)}
+              >
                 <View style={styles.expenseCard}>
                   <View style={styles.expenseDetailsContainer}>
-                    <RoundedSquareIcon 
+                    <RoundedSquareIcon
                       iconName="cash-outline"
-                      iconSize={screenHeight*0.03}
+                      iconSize={screenHeight * 0.03}
                       iconColor="#FFFFFF"
                       backgroundColor="#006D77"
-                      size={screenHeight*0.07}
+                      size={screenHeight * 0.07}
                     />
                     <View style={styles.expenseTextContainer}>
                       <View style={styles.expenseRow}>
-                        <Text style={styles.expenseCategory}>{expense.category}</Text>
-                        <Text style={styles.expensePrice}>SGD {expense.amount}</Text>
+                        <Text style={styles.expenseCategory}>
+                          {expense.category}
+                        </Text>
+                        <Text style={styles.expensePrice}>
+                          SGD {expense.amount}
+                        </Text>
                       </View>
                       <View style={styles.expenseRow}>
                         <Text style={styles.expenseTitle}>{expense.title}</Text>
-                        <Text style={styles.expenseDate}>{new Date(expense.date).toLocaleDateString()}</Text>
+                        <Text style={styles.expenseDate}>
+                          {new Date(expense.date).toLocaleDateString()}
+                        </Text>
                       </View>
                     </View>
                   </View>
@@ -313,22 +339,20 @@ const Expenses = () => {
         )}
       </Animated.View>
 
-      <View style={styles.NavBarContainer}>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Add Expense"
-            onPress={() => setModalVisible(true)}
-            backgroundColor="#F47966"
-            textColor="#FFFFFF"
-            borderRadius={25}
-            width={screenWidth * 0.5}
-            iconName="add"
-            height={screenHeight*0.055}
-            fontSize={screenHeight*0.02}
-            paddingVertical={screenHeight*0.001} 
-          />
-        </View>
-        <NavBar />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Add Expense"
+          onPress={() => setModalVisible(true)}
+          backgroundColor="#F47966"
+          textColor="#FFFFFF"
+          borderRadius={25}
+          width={screenWidth * 0.45}
+          iconName="add"
+          height={screenHeight * 0.065}
+          fontSize={screenHeight * 0.02}
+          paddingVertical={screenHeight * 0.001}
+          // style={{ opacity: 0.9 }}
+        />
       </View>
 
       <AddExpenseModal
@@ -338,11 +362,15 @@ const Expenses = () => {
       />
 
       {/* SetBudgetModal implementation */}
-      <SetBudgetModal 
-        visible={budgetModalVisible} 
-        onClose={() => setBudgetModalVisible(false)} 
-        onSetBudget={setBudget} 
+      <SetBudgetModal
+        visible={budgetModalVisible}
+        onClose={() => setBudgetModalVisible(false)}
+        onSetBudget={setBudget}
       />
+
+      <View style={styles.NavBarContainer}>
+        <NavBar />
+      </View>
     </View>
   );
 };
@@ -357,38 +385,38 @@ const styles = StyleSheet.create({
   },
 
   headerContainer: {
-    flexDirection: 'row', // Aligns the texts in a row
-    alignItems: 'baseline', // Aligns the texts at their baselines
-    justifyContent: 'space-between', // Space between EXPENSES and Sentosa
+    flexDirection: "row", // Aligns the texts in a row
+    alignItems: "baseline", // Aligns the texts at their baselines
+    justifyContent: "space-between", // Space between EXPENSES and Sentosa
     marginBottom: 10, // Adjust margin for spacing below the header
     paddingHorizontal: screenWidth * 0.05, // Added for uniform padding
   },
   header: {
     fontSize: screenHeight * 0.03,
-    fontFamily: 'Nunito_700Bold',
-    color: '#006D77',
+    fontFamily: "Nunito_700Bold",
+    color: "#006D77",
   },
   tripText: {
     fontSize: screenHeight * 0.025, // Adjusted size for better balance with header
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 2, // Adjust bottom margin if needed
   },
   tripContainer: {
-    alignItems: 'center', // Center align Sentosa text and line
+    alignItems: "center", // Center align Sentosa text and line
   },
   line: {
-    width: '100%', // Full width of the parent
+    width: "100%", // Full width of the parent
     height: 2, // Height of the line
-    backgroundColor: '#F47966', // Line color
+    backgroundColor: "#F47966", // Line color
   },
   box: {
-    width: '100%', 
-    height: screenHeight * 0.8, 
-    backgroundColor: '#006D77', 
-    borderTopLeftRadius: screenWidth * 0.05, 
-    borderTopRightRadius: screenWidth * 0.05, 
-    alignItems: 'center', 
-    position: 'relative', 
+    width: "100%",
+    height: screenHeight * 0.8,
+    backgroundColor: "#006D77",
+    borderTopLeftRadius: screenWidth * 0.05,
+    borderTopRightRadius: screenWidth * 0.05,
+    alignItems: "center",
+    position: "relative",
   },
   innerBox: {
     alignItems: "center",
@@ -417,33 +445,33 @@ const styles = StyleSheet.create({
     marginTop: screenHeight * 0.01,
   },
   whiteBox: {
-    position: 'absolute', 
-    top: screenHeight * 0.4, 
+    position: "absolute",
+    top: screenHeight * 0.4,
     left: 0,
     right: 0,
     paddingHorizontal: screenWidth * 0.05,
     paddingTop: screenHeight * 0.02,
-    paddingBottom: screenHeight * 0.1,
-    backgroundColor: '#FFF',
+    paddingBottom: screenHeight * 0.15,
+    backgroundColor: "#FFF",
     borderTopLeftRadius: screenWidth * 0.07,
     borderTopRightRadius: screenWidth * 0.07,
     height: screenHeight * 0.625,
   },
   topLeftText: {
     fontSize: screenHeight * 0.02,
-    fontFamily: 'Nunito_700Bold',
-    color: '#333',
+    fontFamily: "Nunito_700Bold",
+    color: "#333",
     marginBottom: screenHeight * 0.02,
   },
   sortContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: screenHeight * 0.015,
   },
   sortText: {
     fontSize: screenHeight * 0.02,
-    fontFamily: 'Nunito_400Regular',
-    color: '#333',
+    fontFamily: "Nunito_400Regular",
+    color: "#333",
     marginRight: screenWidth * 0.02,
   },
   picker: {
@@ -452,93 +480,100 @@ const styles = StyleSheet.create({
   },
   noExpenses: {
     fontSize: screenHeight * 0.02,
-    fontFamily: 'Nunito_400Regular',
-    color: '#888',
-    textAlign: 'center',
+    fontFamily: "Nunito_400Regular",
+    color: "#888",
+    textAlign: "center",
     marginTop: screenHeight * 0.1,
   },
   expenseCard: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: screenHeight * 0.015,
     paddingHorizontal: screenWidth * 0.02,
     borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
+    borderBottomColor: "#ddd",
   },
   expenseDetailsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   expenseTextContainer: {
     flex: 1,
     marginLeft: screenWidth * 0.02,
   },
   expenseRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   expenseCategory: {
     fontSize: screenHeight * 0.02,
-    fontFamily: 'Nunito_700Bold',
-    color: '#333',
+    fontFamily: "Nunito_700Bold",
+    color: "#333",
   },
   expensePrice: {
     fontSize: screenHeight * 0.02,
-    fontFamily: 'Nunito_700Bold',
-    color: '#333',
+    fontFamily: "Nunito_700Bold",
+    color: "#333",
   },
   expenseTitle: {
     fontSize: screenHeight * 0.018,
-    fontFamily: 'Nunito_400Regular',
-    color: '#666',
+    fontFamily: "Nunito_400Regular",
+    color: "#666",
   },
   expenseDate: {
     fontSize: screenHeight * 0.018,
-    fontFamily: 'Nunito_400Regular',
-    color: '#666',
+    fontFamily: "Nunito_400Regular",
+    color: "#666",
   },
   pieChartContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: screenHeight * 0.4, // Position below the "View Summary" button
     left: 0,
     right: 0,
-    alignItems: 'center',
+    alignItems: "center",
     paddingBottom: screenHeight * 0.02,
   },
   pieChartPlaceholder: {
     fontSize: screenHeight * 0.02,
-    color: '#333',
-    backgroundColor: '#eee',
+    color: "#333",
+    backgroundColor: "#eee",
     width: screenWidth * 0.6,
     height: screenWidth * 0.6,
-    textAlign: 'center',
-    textAlignVertical: 'center',
+    textAlign: "center",
+    textAlignVertical: "center",
     borderRadius: screenWidth * 0.3,
   },
   footerContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
-    alignItems: 'center',
-    backgroundColor: '#FFF',
+    alignItems: "center",
+    backgroundColor: "#FFF",
   },
   buttonContainer: {
-    marginVertical: screenHeight * 0.01,
+    position: "absolute",
+    bottom: 81, // Adjust this as necessary for spacing from the bottom
+    left: 0,
+    right: 0,
+    alignItems: "center", // Centers the button horizontally
   },
-
   deleteContainer: {
-    backgroundColor: '#F47966',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    width: screenWidth*0.2,
-    height: '100%',
-    alignItems: 'center',
+    backgroundColor: "#F47966",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    width: screenWidth * 0.2,
+    height: "100%",
+    alignItems: "center",
   },
   deleteText: {
-    color: 'white', // Set text color to white for visibility
-    fontSize: screenWidth*0.035, // Adjust font size as needed
-    textAlign: 'center', // Center text horizontally
+    color: "white", // Set text color to white for visibility
+    fontSize: screenWidth * 0.035, // Adjust font size as needed
+    textAlign: "center", // Center text horizontally
   },
- 
+  NavBarContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
 });
